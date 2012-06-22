10dbf8bc2e0f4c1a04f4953cf5b92d9bbfd99776
Kenneth Heafield <github@kheafield.com>
2012-06-22 10:15:48 -0400

    Force UTF8 file encoding

--- MEMT/Alignment/match.sh
+++ MEMT/Alignment/match.sh
@@ -3,4 +3,4 @@ loc="$(dirname $0)"
 if [ ! -f "$loc"/MatcherMEMT.class ]; then
   "$loc"/compile.sh || exit 1
 fi
-exec java -cp "$loc":"$loc"/../../Utilities/scoring/meteor-1.0/dist/meteor-1.0/meteor.jar MatcherMEMT "$@"
+exec java -Dfile.encoding=UTF8 -cp "$loc":"$loc"/../../Utilities/scoring/meteor-1.0/dist/meteor-1.0/meteor.jar MatcherMEMT "$@"

a3b1148f8f2b7ecc492025afb02904869889195f
Kenneth Heafield <github@kheafield.com>
2012-08-27 16:16:45 -0400

    Don't output bad alignment on first try

--- MEMT/Alignment/match.sh
+++ MEMT/Alignment/match.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 loc="$(dirname $0)"
 if [ ! -f "$loc"/MatcherMEMT.class ]; then
-  "$loc"/compile.sh || exit 1
+  "$loc"/compile.sh 1>&2 || exit 1
 fi
 exec java -Dfile.encoding=UTF8 -cp "$loc":"$loc"/../../Utilities/scoring/meteor-1.0/dist/meteor-1.0/meteor.jar MatcherMEMT "$@"

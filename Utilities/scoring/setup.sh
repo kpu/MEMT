46fb3c449793eb54eb886f054fbc3087e9dba394
Kenneth Heafield <github@kheafield.com>
2015-03-29 18:29:06 -0400

    Update TERP url / Antonio Toral

--- Utilities/scoring/setup.sh
+++ Utilities/scoring/setup.sh
@@ -11,7 +11,7 @@ function error() { echo Setup failed.; exit 1; }
 [ -f mteval-v13.pl ] || wget ftp://jaguar.ncsl.nist.gov/mt/resources/mteval-v13.pl || error
 [ -f meteor-1.0.tgz ] || wget http://www.cs.cmu.edu/~alavie/METEOR/download/meteor-1.0.tgz || error
 [ -f tercom-0.7.25.tgz ] || wget http://www.cs.umd.edu/~snover/tercom/tercom-0.7.25.tgz || error
-[ -f terp-pt.v1.tgz ] || wget http://www.umiacs.umd.edu/~snover/terp/downloads/terp-pt.v1.tgz || error
+[ -f terp-pt.v1.tgz ] || wget http://web.archive.org/web/20120608122411/http://www.umiacs.umd.edu/~snover/terp/downloads/terp-pt.v1.tgz || error
 trap error ERR
 chmod +x mteval-v13.pl
 tar xzf meteor-1.0.tgz
